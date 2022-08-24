58fac509daaabe2d692fc6cba279f8967db0d6ec
Your Name <you@example.com>
2022-08-24 14:22:16 +0600

    new setup done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Header from '../../Shared/Header';
-import Background from './Background';
-import Homeservice1 from './HomeService/Homeservice1';
-import Homeservice2 from './HomeService/Homeservice2';
+
 
 const Home = () => {
     return (
         <div>
-            <Header></Header>
-            <Background></Background>
-            <Homeservice1></Homeservice1>
-            <Homeservice2></Homeservice2>
+            <h1> Our Home page</h1>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../../Shared/Header';
 
 const Services = () => {
     return (
         <div>
-            <Header></Header>
             <h1>Our serrvices</h1>
         </div>
     );
