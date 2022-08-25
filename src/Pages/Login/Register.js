5c8ede9124055eb16a606ef0f9fe067fb227c194
mnaofficial <mna.official92@gmail.com>
2022-08-26 01:02:33 +0600

    register page primary setup

--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import registerImg from './register.png'
 
 const Register = () => {
     return (
         <div>
-            <h2>Please Register</h2>
+            <h2 className="text-center text-2">Please Register</h2>
+            <div class="hero">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <div class="text-center lg:text-left">
+                        <img src={registerImg} alt='Register' class="max-w-sm rounded-lg " />
+                    </div>
+                    <div class="card flex-shrink-0 w-full max-w-sm bg-base-100">
+                        <div class="card-body">
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" class="input input-bordered" />
+                            </div>
+                            <div class="form-control">
+                                <label class="label">
+                                    <span class="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" class="input input-bordered" />
+                                <label class="label">
+                                    <a href="#home" class="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div class="form-control mt-6">
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
